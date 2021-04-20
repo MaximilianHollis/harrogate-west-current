e36c8dd9e499bdc876039e9381dab4b518fb938a
Maximilian Hollis <hollismaximilian@gmail.com>
2021-04-20 17:48:59 -0400

    removed faulty routing

--- next.config.js
+++ next.config.js
@@ -1,12 +1,3 @@
 // next.config.js
 const withImages = require('next-images')
 module.exports = withImages()
-
-module.exports = {
-    trailingSlash: true,
-    exportPathMap: function() {
-      return {
-        '/': { page: '/' }
-      };
-    }
-  };
