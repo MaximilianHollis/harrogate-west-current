c0bfa8bef96cf656ec997c31a82df90eeccdb6f8
Maximilian Hollis <hollismaximilian@gmail.com>
2021-04-20 17:47:14 -0400

    Update build config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,12 @@
 // next.config.js
 const withImages = require('next-images')
 module.exports = withImages()
+
+module.exports = {
+    trailingSlash: true,
+    exportPathMap: function() {
+      return {
+        '/': { page: '/' }
+      };
+    }
+  };
